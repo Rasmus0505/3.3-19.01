8ab9d70dde8d98416c1b0804ff2f53f751212de5
Rasmus0505 <992519622@qq.com>
2026-03-25 01:40:50 +0800

    修复白屏

--- ops/sql/app_to_public_schema_rollback.sql
+++
@@ -1,9 +0,0 @@
-BEGIN;
-
-ALTER TABLE IF EXISTS app.users SET SCHEMA public;
-ALTER TABLE IF EXISTS app.lessons SET SCHEMA public;
-ALTER TABLE IF EXISTS app.lesson_sentences SET SCHEMA public;
-ALTER TABLE IF EXISTS app.lesson_progress SET SCHEMA public;
-ALTER TABLE IF EXISTS app.media_assets SET SCHEMA public;
-
-COMMIT;
