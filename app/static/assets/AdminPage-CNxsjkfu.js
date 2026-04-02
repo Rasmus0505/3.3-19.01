d1ada2b3de20bdb7ce12df45894bcb160e1c6a90
Rasmus0505 <992519622@qq.com>
2026-04-02 22:57:56 +0800

    1

--- app/static/assets/AdminPage-CNxsjkfu.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-0F6CDbXC.js";import{L as n}from"./LearningShell-BJ3AfsST.js";function e(){return r.jsx(n,{})}export{e as AdminPage};
--- app/static/assets/LearningPage-BH7VfDH_.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-0F6CDbXC.js";import{L as n}from"./LearningShell-BJ3AfsST.js";function t(){return r.jsx(n,{})}export{t as LearningPage};
